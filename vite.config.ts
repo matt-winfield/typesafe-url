c97ce9b12c79f1662be6b2ae700a5c6c2ab5bc1a
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2024-05-29 19:39:01 +0100

    Update build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
+  plugins: [
+    dts({
+      entryRoot: "src",
+      exclude: ["**/*.test.ts", "**/*.test.tsx"],
+    }),
+  ],
   build: {
     lib: {
       entry: "src/index.ts",
