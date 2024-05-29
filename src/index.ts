fde7c4df5291f96c57c03a10c95c9555dc996ad0
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2024-05-29 19:57:48 +0100

    Add query string param serialization

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { ZodObject } from "zod";
+import { ZodObject, z } from "zod";
 
 export interface PathOptions<
   TParams extends ZodObject<any>,
@@ -8,12 +8,66 @@ export interface PathOptions<
   query?: TQuery;
 }
 
-export const path = <
+export type PathDefinition<
+  TParams extends ZodObject<any>,
+  TQuery extends ZodObject<any>,
+> = {
+  path: string;
+  options: PathOptions<TParams, TQuery>;
+};
+
+export const definePath = <
   TParams extends ZodObject<any>,
   TQuery extends ZodObject<any>,
 >(
   path: string,
   options: PathOptions<TParams, TQuery>,
 ) => {
-  return path;
+  return {
+    path,
+    options,
+  };
+};
+
+export const path = <
+  TParams extends ZodObject<any>,
+  TQuery extends ZodObject<any>,
+>(
+  pathDefinition: PathDefinition<TParams, TQuery>,
+  params: { params?: z.infer<TParams>; query?: z.infer<TQuery> },
+) => {
+  const { path, options } = pathDefinition;
+  const { params: pathParams, query } = params;
+
+  const output = path;
+
+  if (query) {
+    const serializedQuery = serializeQueryParams(query);
+    return `${output}?${serializedQuery}`;
+  }
+
+  return output;
+};
+
+const serializeQueryParams = (query: Record<string, any>) => {
+  return Object.keys(query)
+    .map((key) => {
+      const value = query[key];
+      if (typeof value === "object") {
+        return serializeObject(value, key);
+      }
+      return `${key}=${value}`;
+    })
+    .join("&");
+};
+
+const serializeObject = (obj: Record<string, any>, key: string) => {
+  return Object.keys(obj)
+    .map((nestedKey) => {
+      if (typeof obj[nestedKey] === "object") {
+        return serializeObject(obj[nestedKey], `${key}[${nestedKey}]`);
+      }
+      return `${key}[${nestedKey}]=${obj[nestedKey]}`;
+    })
+    .join("&");
 };
--- src/typesafe-path.test.ts
+++ src/typesafe-path.test.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
-import { path } from ".";
+import { definePath, path } from ".";
 
 describe("typesafe-path", () => {
-  it("should return a string", () => {
-    const test = path("/test/path", {
+  it("should serialize query params", () => {
+    const testPath = definePath("/test/path", {
       query: z.object({
         test: z.string(),
         something: z.number(),
@@ -12,5 +12,71 @@ describe("typesafe-path", () => {
         }),
       }),
     });
+
+    const result = path(testPath, {
+      query: {
+        test: "test",
+        something: 123,
+        blah: {
+          nested: "nested",
+        },
+      },
+    });
+
+    expect(result).toBe(
+      "/test/path?test=test&something=123&blah[nested]=nested",
+    );
+  });
+
+  it("should serialize deeply nested query params", () => {
+    const testPath = definePath("/test/path", {
+      query: z.object({
+        nested: z.object({
+          deeply: z.object({
+            deeper: z.object({
+              deepest: z.string(),
+            }),
+          }),
+          test: z.object({
+            nested: z.string(),
+          }),
+        }),
+      }),
+    });
+
+    const result = path(testPath, {
+      query: {
+        nested: {
+          deeply: {
+            deeper: {
+              deepest: "nested",
+            },
+          },
+          test: {
+            nested: "nested",
+          },
+        },
+      },
+    });
+
+    expect(result).toBe(
+      "/test/path?nested[deeply][deeper][deepest]=nested&nested[test][nested]=nested",
+    );
+  });
+
+  it("should serialize query params with arrays", () => {
+    const testPath = definePath("/test/path", {
+      query: z.object({
+        test: z.array(z.string()),
+      }),
+    });
+
+    const result = path(testPath, {
+      query: {
+        test: ["test", "test2"],
+      },
+    });
+
+    expect(result).toBe("/test/path?test[0]=test&test[1]=test2");
   });
 });
